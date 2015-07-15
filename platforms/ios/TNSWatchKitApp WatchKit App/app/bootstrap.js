d944ddb3b58fa5b7a8ca962b1795c9c4c94364cb
Antony Bello <antony.bello@pomona.edu>
2015-07-15 16:10:13 -0700

    Changing config

--- platforms/ios/TNSWatchKitApp WatchKit App/app/bootstrap.js
+++ platforms/ios/TNSWatchKitApp WatchKit App/app/bootstrap.js
@@ -1,68 +1,67 @@
 console.log("Hello World!");
-
-var taps = 47;
+var taps = 0;
 var InterfaceController = WKInterfaceController.extend({
-                                                       awakeWithContext: function(context) {
-                                                       this.super.awakeWithContext(context);
-                                                       console.log("InterfaceController: awakeWithContext");
-                                                       },
-                                                       willActivate: function() {
-                                                       this.super.willActivate();
-                                                       console.log("InterfaceController: willActivate");
-                                                       },
-                                                       didDeactivate: function() {
-                                                       this.super.didDeactivate();
-                                                       console.log("InterfaceController: didDeactivate");
-                                                       },
-                                                       tapIncrement: function() {
-                                                       console.log("Here!");
-                                                       taps++;
-                                                       this._tapsLabel.setText("Taps: " + taps);
-                                                       },
-                                                       tapsLabel: function() {
-                                                       return this._tapsLabel;
-                                                       },
-                                                       "setTapsLabel:": function(value) {
-                                                       this._tapsLabel = value;
-                                                       console.log("Set label: " + value);
-                                                       }
-                                                       }, {
-                                                       name: "InterfaceController",
-                                                       exposedMethods: {
-                                                       tapIncrement: { returns: interop.types.void, params: [] },
-                                                       tapsLabel: { returns: interop.types.id, params: [] },
-                                                       "setTapsLabel:": { returns: interop.types.void, params: [interop.types.id] }
-                                                       }
-                                                       });
+awakeWithContext: function(context) {
+this.super.awakeWithContext(context);
+console.log("InterfaceController: awakeWithContext");
+},
+willActivate: function() {
+this.super.willActivate();
+console.log("InterfaceController: willActivate");
+},
+didDeactivate: function() {
+this.super.didDeactivate();
+console.log("InterfaceController: didDeactivate");
+},
+tapIncrement: function() {
+                                                       console.log(taps);
+taps++;
+this._tapsLabel.setText("Taps: " + taps);
+},
+tapsLabel: function() {
+return this._tapsLabel;
+},
+"setTapsLabel:": function(value) {
+this._tapsLabel = value;
+console.log("Set label: " + value);
+}
+}, {
+name: "InterfaceController",
+exposedMethods: {
+tapIncrement: { returns: interop.types.void, params: [] },
+tapsLabel: { returns: interop.types.id, params: [] },
+"setTapsLabel:": { returns: interop.types.void, params: [interop.types.id] }
+}
+});
 
 var NotificationController = WKUserNotificationInterfaceController.extend({
-                                                                          willActivate: function() {
-                                                                          this.super.willActivate();
-                                                                          console.log("NotificationController: willActivate");
-                                                                          },
-                                                                          didDeactivate: function() {
-                                                                          this.super.didDeactivate();
-                                                                          console.log("NotificationController: didDeactivate");
-                                                                          }
-                                                                          }, {
-                                                                          name: "NotificationController"
-                                                                          });
+                  willActivate: function() {
+                  this.super.willActivate();
+                  console.log("NotificationController: willActivate");
+                  },
+                  didDeactivate: function() {
+                  this.super.didDeactivate();
+                  console.log("NotificationController: didDeactivate");
+                  }
+                  }, {
+                  name: "NotificationController"
+                  });
 
 var GlanceController = WKInterfaceController.extend({
-                                                    awakeWithContext: function(context) {
-                                                    this.super.awakeWithContext(context);
-                                                    console.log("GlanceController: awakeWithContext");
-                                                    },
-                                                    willActivate: function() {
-                                                    this.super.willActivate();
-                                                    console.log("GlanceController: willActivate");
-                                                    },
-                                                    didDeactivate: function() {
-                                                    this.super.didDeactivate();
-                                                    console.log("GlanceController: didDeactivate");
-                                                    }
-                                                    }, {
-                                                    name: "GlanceController"
-                                                    });
+awakeWithContext: function(context) {
+this.super.awakeWithContext(context);
+console.log("GlanceController: awakeWithContext");
+},
+willActivate: function() {
+this.super.willActivate();
+console.log("GlanceController: willActivate");
+},
+didDeactivate: function() {
+this.super.didDeactivate();
+console.log("GlanceController: didDeactivate");
+}
+}, {
+name: "GlanceController"
+});
 
 console.log("declared controllers");
