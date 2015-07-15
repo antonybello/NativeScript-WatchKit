1f08d99903863417c2f92ced67edf23cee403160
Antony Bello <antony.bello@pomona.edu>
2015-07-15 14:45:36 -0700

    Added Everything

---
+++ platforms/ios/TNSWatchKitApp WatchKit Extension/NSObject+main_m.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+main_m.h
+//  TNSWatchKitApp
+//
+//  Created by Antony Bello on 7/15/15.
+//  Copyright (c) 2015 Telerik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (main_m)
+
+@end
---
+++ platforms/ios/TNSWatchKitApp WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  TNSWatchKitApp WatchKit Extension
+//
+//  Created by Antony Bello on 7/15/15.
+//  Copyright (c) 2015 Telerik. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
